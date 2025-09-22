4667c0fd4e7b9100030855f3f04963c88896bd40
amran1451 <aeamarda@gmail.com>
2025-09-22 20:05:21 +0300

    Fix post frame callback mounted check

--- lib/utils/ref_postframe.dart
+++ lib/utils/ref_postframe.dart
@@ -4,7 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 extension RefPostFrame on WidgetRef {
   void postFrame(void Function() fn) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
-      if (mounted) fn();
+      if (context.mounted) fn();
     });
   }
 }
